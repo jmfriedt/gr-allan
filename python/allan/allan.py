c974189afb05b471683b7189134f12b98aabc67a
Jean-Michel Friedt <friedtj@free.fr>
2023-06-11 20:50:43 +0200

    set dataset as frequency and not phase

--- python/allan/allan.py
+++ python/allan/allan.py
@@ -59,7 +59,7 @@ class allan(gr.sync_block, AllanAff): # Display):
     """
     docstring for block allan
     """
-    def __init__(self, allan_type=0, allan_inc=1000, textdisplay=False, backgroundColor='white', parent=None):
+    def __init__(self, allan_type=0, input_type=False, allan_inc=1000, textdisplay=False, backgroundColor='white', parent=None):
         gr.sync_block.__init__(self,
             name="allan",
             in_sig=[numpy.float32],
@@ -68,8 +68,9 @@ class allan(gr.sync_block, AllanAff): # Display):
         self.allan_type=allan_type
         self.allan_inc=allan_inc
         self.textdisplay=textdisplay
+        self.input_type=input_type
         print("Init "+str(allan_type),flush=True)
-        self.dev_rt = at.realtime.tdev_realtime(tau0=1.0,auto_afs=True)
+        self.dev_rt = at.realtime.tdev_realtime(tau0=1.0,auto_afs=True)  # default
         if allan_type==0:
             self.dev_rt = at.realtime.tdev_realtime(tau0=1.0,auto_afs=True)
         if allan_type==1:
@@ -84,7 +85,10 @@ class allan(gr.sync_block, AllanAff): # Display):
         # signal processing here
         for x in in0:
             self.counter=self.counter+1
-            self.dev_rt.add_phase(x)
+            if self.input_type==False:
+                self.dev_rt.add_frequency(x)
+            else:
+                self.dev_rt.add_phase(x)
             if (self.counter>self.allan_inc):
                 self.counter=0
                 taus=self.dev_rt.taus()
