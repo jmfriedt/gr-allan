8db4ad93b4c25b384f6348aa8525a578c9259e6d
Jean-Michel Friedt <friedtj@free.fr>
2023-06-11 17:13:38 +0200

    validation sample and option for display Allan deviation in console

--- python/allan/allan.py
+++ python/allan/allan.py
@@ -55,7 +55,7 @@ class allan(gr.sync_block, AllanAff): # Display):
     """
     docstring for block allan
     """
-    def __init__(self, allan_type=0, allan_inc=1000, backgroundColor='white', parent=None):
+    def __init__(self, allan_type=0, allan_inc=1000, textdisplay=False, backgroundColor='white', parent=None):
         gr.sync_block.__init__(self,
             name="allan",
             in_sig=[numpy.float32],
@@ -63,6 +63,7 @@ class allan(gr.sync_block, AllanAff): # Display):
         self.counter=0
         self.allan_type=allan_type
         self.allan_inc=allan_inc
+        self.textdisplay=textdisplay
         print("Init "+str(allan_type),flush=True)
         self.dev_rt = at.realtime.tdev_realtime(tau0=1.0,auto_afs=True)
         if allan_type==0:
@@ -81,10 +82,13 @@ class allan(gr.sync_block, AllanAff): # Display):
         for x in in0:
             self.counter=self.counter+1
             self.dev_rt.add_phase(x)
-            taus=self.dev_rt.taus()
-            devs=self.dev_rt.devs()
             if (self.counter>self.allan_inc):
                 self.counter=0
+                taus=self.dev_rt.taus()
+                devs=self.dev_rt.devs()
+                if (self.textdisplay==True):
+                    print(taus)
+                    print(devs)
                 super().setValue(taus,devs)
         return len(input_items[0])
 
